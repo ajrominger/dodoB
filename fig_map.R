d1eb5136a597e2e0928d6baffa0e961d200ef836
ajrominger <ajrominger@gmail.com>
2018-02-16 11:00:46 -0700

    got map fig how i like it

--- fig_map.R
+++ fig_map.R
@@ -1,5 +1,5 @@
-## modified from stackoverflow: 
-## https://stackoverflow.com/questions/10620862/use-different-center-than-the-prime-meridian-in-plotting-a-world-map
+## mapping functions modified from stackoverflow qustion 10620862
+## as answered by Josh O'Brien
 
 library(sp)
 library(maps)
@@ -13,6 +13,7 @@ makeSLmap <- function() {
     wrld <- map("world", interior = FALSE, plot=FALSE,
                 xlim = c(-179, 179), ylim = c(-89, 89))
     wrld_p <- pruneMap(wrld, xlim = c(-179, 179))
+    
     map2SpatialLines(wrld_p, proj4string = llCRS)
 }
 
@@ -75,9 +76,52 @@ x@data <- data.frame(x@data[, 1, drop = FALSE],
 m <- Recenter(170)
 x <- spTransform(x, CRS(proj4string(m)))
 
-pdf('foo.pdf')
-plot(m, xlim = c(-1e+07, 2e+07), col = 'gray')
-plot(x, col = x$col, add = TRUE)
+pdf('fig_map.pdf', width = 10, height = 4)
+layout(matrix(1:2, ncol = 2), widths = c(2, 1))
+
+## the actual map
+par(mar = rep(0.5, 4))
+plot(m, xlim = c(-1e+07, 2e+07), col = 'gray', lwd = 0.75)
+plot(x, col = colAlpha(x$col, 0.7), border = x$col, add = TRUE, lwd = 2)
 polygon(c(2.1e+07, 1.5e+07, 2.1e+07), c(-3e+06, 4e+06, 7.5e+06), col = 'white', 
         border = 'white')
+
+## the scale plot
+
+aScaleMax <- tapply(x$area, x$project, mean)
+aScaleMin <- c(10^5, 10^4, 10^2, 10, 10^5)
+aResoMax <- aScaleMax / 10^3
+aResoMin <- c(100, 100, 1, 1, 50)
+aCol <- tapply(x$col, x$project, head, n = 1)
+
+smoothPoly <- function(x, y, ...) {
+    xpr <- exp(spline(1:length(x), log(x), n = 50)[[2]])
+    ypr <- exp(spline(1:length(y), log(y), n = 50)[[2]])
+    
+    polygon(xpr, ypr, ...)
+}
+
+par(mar = c(4, 2.5, 2, 1.5) + 0.5, mgp = c(2, 0.5, 0))
+
+plot(1, log = 'xy', axes = FALSE, type = 'n',
+     xlim = c(10^1, 10^7), ylim = c(0.5, 10^6), 
+     xlab = 'Spatial scale', ylab = 'Spatial resolution')
+
+for(i in 1:length(aScaleMax)) {
+    # polygon(c(aScaleMin[i], aScaleMax[i], aScaleMax[i]), 
+    #         c(aResoMin[i], aResoMax[i], aScaleMax[i] / 10), 
+    #         col = colAlpha(aCol[i], 0.5), border = aCol[i], lwd = 3)
+    smoothPoly(c(aScaleMin[i], aScaleMax[i], aScaleMax[i], aScaleMin[i]), 
+               c(aResoMin[i], aResoMax[i], aScaleMax[i] / 10, aResoMin[i]), 
+               col = colAlpha(aCol[i], 0.5), border = aCol[i], lwd = 2)
+}
+
+axis(1, at = 10^c(1, 4, 7), labels = c(expression(10~km^2), 
+                                       expression(10^4~km^2),
+                                       expression(10^7~km^2)))
+axis(2, at = 10^c(0, 3, 6), labels = c(expression(1~km^2), 
+                                       expression(10^3~km^2),
+                                       expression(10^6~km^2)))
+box()
+
 dev.off()
